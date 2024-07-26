4d18aac69cf25240acf063af06559ca1dd6a7c32
robbintj <robbintj@hotmail.com>
2024-07-26 13:52:40 -0300

    implemtar data e combo-box no register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,6 +8,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-register',
@@ -22,7 +25,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatSelectModule,
     MatOptionModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [MockDataService]
 })
@@ -31,14 +36,14 @@ export class RegisterComponent implements OnInit {
 
   fields = [
     { name: 'id', label: 'ID', type: 'text', hint: 'Digite o ID', required: true },
-    { name: 'paredeFornalha', label: 'Parede Fornalha', type: 'text', hint: 'Descrição da parede', required: true },
+    { name: 'paredeFornalha', label: 'Parede Fornalha', type: 'select', options: ['TRASEIRA', 'LESTE', 'OESTE', 'LESTE'], hint: 'Selecione a Parede Fornalha', required: true },
     { name: 'local', label: 'Local', type: 'text', hint: 'Local do tubo', required: true },
     { name: 'nTubo', label: 'Nº Tubo', type: 'text', hint: 'Número do tubo', required: true },
     { name: 'nTuboAdjacente', label: 'Nº Tubo adjacente', type: 'text', hint: 'Número do tubo adjacente', required: false },
-    { name: 'elevacaoInferior', label: 'Elevação Inferior', type: 'number', hint: 'Elevação inferior', required: true },
-    { name: 'elevacaoSuperior', label: 'Elevação Superior', type: 'number', hint: 'Elevação superior', required: true },
+    { name: 'elevacaoInferior', label: 'Elevação Inferior', type: 'select', options: ['0', '1851', '3702','5553', '7404', '9255', '11106', '12957', '14808', '16659', '18510', '20361', '22212'], hint: 'Selecione a Elevação inferior', required: true },
+    { name: 'elevacaoSuperior', label: 'Elevação Superior', type: 'select', options: ['1850', '3701', '5552', '7403', '9254', '11105', '12956', '14807', '16658', '18509', '20360', '22211', '24062'], hint: 'Selecione a Elevação Superior', required: true },
     { name: 'dimensao', label: 'Dimensão (mm)', type: 'number', hint: 'Dimensão em milímetros', required: true },
-    { name: 'escopo', label: 'Escopo', type: 'text', hint: 'Escopo do projeto', required: false },
+    { name: 'escopo', label: 'escopo', type: 'select', options: ['REVEST. DURO', 'REVEST. NORMAL', 'TROCAR TUBO', 'REVEST. ALETA'], hint: 'Selecione o Escopo do projeto', required: true },
     { name: 'lado', label: 'Lado', type: 'select', options: ['INTERNO', 'EXTERNO'], hint: 'Selecione o lado', required: true },
     { name: 'tipoEscopo', label: 'Tipo Escopo', type: 'text', hint: 'Tipo de escopo', required: false },
     { name: 'liberadoGeral', label: 'Liberado geral', type: 'text', hint: 'Liberado geral', required: false },
@@ -53,7 +58,7 @@ export class RegisterComponent implements OnInit {
     { name: 'lpH2F', label: 'LP H2F', type: 'number', hint: 'LP H2F', required: false },
     { name: 'liberadoH2F', label: 'Liberado H2F', type: 'number', hint: 'Liberado H2F', required: false },
     { name: 'dataLiberadoH2F', label: 'Data Liberado H2F', type: 'date', hint: 'Data liberado H2F', required: false },
-    { name: 'status', label: 'Status', type: 'text', hint: 'Status do item', required: false },
+    { name: 'status', label: 'Status', type: 'select', options: ['PENDENTE', 'ANDAMENTO', 'CONCLUÍDO'], hint: 'Selecione o Status do item', required: true },
     { name: 'observacaoAlumar', label: 'Observação ALUMAR', type: 'text', hint: 'Observação ALUMAR', required: false },
     { name: 'observacaoMKS', label: 'Observação MKS', type: 'text', hint: 'Observação MKS', required: false },
     { name: 'observacaoH2F', label: 'Observação H2F', type: 'text', hint: 'Observação H2F', required: false }
