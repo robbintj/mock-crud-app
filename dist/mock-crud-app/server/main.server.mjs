c3b42fbc4a1de2ac004a475edfb2aa0e67805c8e
robbintj <robbintj@hotmail.com>
2024-07-26 13:55:47 -0300

    versao 1.1.0

--- dist/mock-crud-app/server/main.server.mjs
+++ dist/mock-crud-app/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QZ7SXOIK.mjs";import"./chunk-DGEOP2QA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-V67C6VDP.mjs";import"./chunk-S6PWRG64.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
