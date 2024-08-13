015ecf2a1b3a3fd865fccd3f119aa73ead5a5763
robbintj <robbintj@hotmail.com>
2024-08-13 12:14:09 -0300

    Adiciona arquivos e diretórios ao controle de versão

---
+++ mks-backend/mks/src/main/java/com/sistema/mks/MksApplication.java
@@ -0,0 +1,11 @@
+package com.sistema.mks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MksApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MksApplication.class, args);
+	}
+}
